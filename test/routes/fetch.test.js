4bb8664910ba987404c9ce19bd56278be8557a39
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-20 12:14:14 +0530

    notes is now an array, fixed tests

--- test/routes/fetch.test.js
+++ test/routes/fetch.test.js
@@ -11,17 +11,14 @@ describe('The fetch route should return a JSON object which has', () => {
   it('a length property', () => {
     expect(notes.length).toBeDefined();
   });
-  it('length+1 keys', () => {
-    expect(Object.keys(notes).length).toBe(notes.length + 1);
-  });
   it('objects containing id', () => {
-    expect(notes['0'].id).toBeDefined();
+    expect(notes[0].id).toBeDefined();
   });
   it('objects containing title', () => {
-    expect(notes['0'].title).toBeDefined();
+    expect(notes[0].title).toBeDefined();
   });
   it('objects containing body', () => {
-    expect(notes['0'].body).toBeDefined();
+    expect(notes[0].body).toBeDefined();
   });
 });
 
